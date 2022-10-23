c828cadd4141f5cc82c6c02b4c68bb34bd5d615e
Bruno Martins <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-10-23 09:17:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ for (index, row) in student_data_frame.iterrows():
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
+alfabeto_data_frame = pandas.DataFrame("nato_phonetic_alphabet.csv") 
+print(alfabeto_data_frame)
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 
