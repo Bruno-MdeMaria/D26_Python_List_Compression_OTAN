55ff9ecfa29e75c4040649558e61ced26eeb7dcb
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-10-25 01:12:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import pandas
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv") 
 dicio = {row.letter: row.code for (index, row) in data.iterrows()}  #a linha index... para receber a coluna (row) cjamda letter e a coluna chamada code.
-print(dicio)
 
 palavra = input("Escreva uma palavra: ").upper()
 soletr = [dicio[letra] for letra in palavra]
 print(soletr)
-
 
 
 
