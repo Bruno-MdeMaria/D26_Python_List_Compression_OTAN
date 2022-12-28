8a46714e754c81a64a4df2d5cf56cd1cb0dda1b6
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-12-28 08:09:55 -0300

    Acrescentado tratamento de exceções
    Acrescentado tratamento de exceções:
    try
    excepct
    else
    e criado função para reiniciar o programa quando encontrado uma except

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 dicio = {row.letter: row.code for (index, row) in data.iterrows()}  #a linha index... para receber a coluna (row) cjamda letter e a coluna chamada code.
 
 print(dicio)
-palavra = input("Escreva uma palavra: ").upper()
-soletr = [dicio[letra] for letra in palavra]
-print(soletr)
+#adicionado tratamento de exceção:
+
+def criador_phonetic():
+    palavra = input("Escreva uma palavra: ").upper()
+    try:    #tentativa:
+        soletr = [dicio[letra] for letra in palavra]
+    except KeyError:  #exceção que queremos apanhar é uma keyerror.
+        print("Desculpe, apenas letras do alfabeto, por favor.")
+        criador_phonetic()
+    else:#se não ocorrer uma exceção o programa deve apresentar o resultado:
+        print(soletr)
+criador_phonetic()
 
 
 
